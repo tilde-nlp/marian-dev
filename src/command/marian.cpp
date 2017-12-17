fed4901805fc8d852136aea88f55a2e141a01bd0
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2017-12-17 09:17:43 +0000

    attempt to merge with master

---
+++ src/command/marian.cpp
@@ -0,0 +1,27 @@
+#include "marian.h"
+
+#include "training/graph_group_async.h"
+#include "training/graph_group_async_drop.h"
+#include "training/graph_group_singleton.h"
+#include "training/graph_group_sync.h"
+#include "training/training.h"
+
+int main(int argc, char** argv) {
+  using namespace marian;
+
+  auto options = New<Config>(argc, argv);
+  auto devices = options->get<std::vector<size_t>>("devices");
+
+  if(devices.size() > 1) {
+    if(options->get<bool>("sync-sgd"))
+      New<Train<SyncGraphGroup>>(options)->run();
+    else if(options->get<float>("gradient-dropping") > 0.0)
+      New<Train<AsyncGraphGroupDrop>>(options)->run();
+    else
+      New<Train<AsyncGraphGroup>>(options)->run();
+  } else {
+    New<Train<SingletonGraph>>(options)->run();
+  }
+
+  return 0;
+}
