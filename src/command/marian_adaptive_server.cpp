935060f64aa46a4679d9990daf4a1cb2f83a1094
Rihards Krišlauks <rihards.krislauks@gmail.com>
2018-09-06 16:30:19 +0300

    Add preliminary parsing code

--- src/command/marian_adaptive_server.cpp
+++ src/command/marian_adaptive_server.cpp
@@ -1,8 +1,33 @@
 #include "marian.h"
 #include "translator/beam_search.h"
 #include "translator/translator.h"
+#include "data/adaptation_set.h"
 
 #include "3rd_party/simple-websocket-server/server_ws.hpp"
+
+AdaptationSet parse(std::string message){
+  //Parse the translation request (message) in JSON.
+  YAML::Node message_node = YAML::Load(input);
+  if (!message_node["text"]) {
+    //TODO: Should we handle malformed data?
+  }
+  std::string text = message_node["text"].as<std::string>();
+  std::vector<std::string> context_sources;
+  std::vector<std::string> context_targets;
+  //Check if context exists in the JSON and read it.
+  if (message_node["context"]) {
+    YAML::Node contexts_node = message_node["context"];
+    for (std::size_t i=0; i < contexts_node.size(); i++) {
+      YAML::Node context_node = contexts_node[i];
+      if (context_node["source"] && context_node["target"]) {
+        context_sources.push_back(context_node["source"].as<std::string>());
+        context_targets.push_back(context_node["target"].as<std::string>());
+      }
+    }
+  }
+  return AdaptationSet(text, context_sources, context_targets);
+  // TODO: member initialisation is probably wrong here
+}
 
 typedef SimpleWeb::SocketServer<SimpleWeb::WS> WsServer;
 
@@ -28,7 +53,7 @@ int main(int argc, char **argv) {
 
     auto send_stream = std::make_shared<WsServer::SendStream>();
     boost::timer::cpu_timer timer;
-    for(auto &transl : task->run({message_str})) {
+    for(auto &transl : task->run({parse(message_str)})) {
       LOG(info, "Best translation: {}", transl);
       *send_stream << transl << std::endl;
     }
